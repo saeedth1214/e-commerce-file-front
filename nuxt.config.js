913c114ba4c18cc3ddd5f886eac001cafb11217d
saeedth1214 <52350203+saeedth1214@users.noreply.github.com>
2022-12-23 19:23:03 +0330

    remove rewright

--- nuxt.config.js
+++ nuxt.config.js
@@ -228,7 +228,6 @@ export default {
   proxy: {
      '/api/': {
      target: 'https://api-ecommerce-file.iran.liara.run/api',
-      pathRewrite: {'^/api/': 'https://front-ecommerce-file.iran.liara.run'},
       changeOrigin: true
     }
 },

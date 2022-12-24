d60357e3463c142757fae2d95c1723e1750be98d
saeedth1214 <52350203+saeedth1214@users.noreply.github.com>
2022-12-24 16:30:23 +0330

    change proxy config

--- nuxt.config.js
+++ nuxt.config.js
@@ -227,23 +227,15 @@ export default {
   proxy: {
   'frontend/': {
       target: 'https://api-ecommerce-file.iran.liara.run/api/frontend/',
-//       pathRewrite: {'^/frontend/': ''},
-      changeOrigin: true
     },
      'panel/': {
       target: 'https://api-ecommerce-file.iran.liara.run/api/panel/',
-//       pathRewrite: {'^/panel/': ''},
-      changeOrigin: true
     },
     'auth/': {
       target: 'https://api-ecommerce-file.iran.liara.run/api/auth/',
-//       pathRewrite: {'^/auth/': ''},
-      changeOrigin: true
     },
     'user/': {
       target: 'https://api-ecommerce-file.iran.liara.run/api/user/',
-//       pathRewrite: {'^/user/': ''},
-      changeOrigin: true
     },
     
 },

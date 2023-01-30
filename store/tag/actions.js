37d87195ed8fff460dd5cb03fc8e896498a3bc89
saeedsoltani <m.h.soltani1214@gmail.com>
2023-01-30 18:11:24 +0330

    bug : change per_page  tags in landing page to 5

--- store/tag/actions.js
+++ store/tag/actions.js
@@ -1,7 +1,7 @@
 export default {
   async fetchTagsInLandingPage({ commit, state }) {
     let params = {};
-    params["per_page"] = 15;
+    params["per_page"] = 5;
     let tags = await this.$axios
       .get("frontend/tags", { params })
       .then((res) => {

f593dfa4c94a65f03bf8f27a59fa6de264ae22d7
saeedsoltani <m.h.soltani1214@gmail.com>
2023-02-04 22:52:45 +0330

    bug : fix bug when user click on a tag in landing page

--- components/front/TheSearchByTag.vue
+++ components/front/TheSearchByTag.vue
@@ -1,14 +1,16 @@
 <template>
   <v-col cols="12">
-    <div class="tags">
-      <nuxt-link
-        v-for="tag in tags"
-        :key="tag.id"
-        :to="{ path: 'front/files', query: { searchByTag: tag.name } }"
-      >
-        {{ tag.name }}
-        <v-icon color="#fff">mdi-magnify</v-icon>
-      </nuxt-link>
+    <div class="wrapper-tags">
+      <div class="tags">
+        <nuxt-link
+          v-for="tag in tags"
+          :key="tag.id"
+          :to="{ path: 'front/files', query: { searchByTag: tag.name } }"
+        >
+          {{ tag.name }}
+          <v-icon color="#fff">mdi-magnify</v-icon>
+        </nuxt-link>
+      </div>
     </div>
   </v-col>
 </template>
@@ -31,14 +33,22 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+.wrapper-tags {
+  width: 600px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0px auto;
+  padding: 0.5rem;
+}
 .tags {
   display: flex;
   margin: 0px auto;
   justify-content: center;
+  width: 500px;
 
   a {
     color: #fff;
-    font-size: 1rem;
     text-decoration: none;
     padding: 0.5rem;
     border: 1px solid #fff;
@@ -50,11 +60,22 @@ export default {
     text-align: center;
     cursor: pointer;
   }
+}
 
-  @media screen and (max-width: 400px) {
-    span {
-      font-size: 10px;
-      padding: 0.2rem;
+@media screen and (max-width: 500px) {
+  .wrapper-tags {
+    width: 95%;
+    overflow-x: scroll;
+    // border: 1px solid red;
+    height: 50px;
+    position: relative;
+    .tags {
+      width: 500px;
+      position: absolute;
+      left: 0px;
+      a {
+        width: 140px;
+      }
     }
   }
 }

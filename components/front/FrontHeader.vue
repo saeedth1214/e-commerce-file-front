caf9f02fa422d6d1fa7ef92918b92eac824295c7
saeedsoltani <m.h.soltani1214@gmail.com>
2023-02-22 11:24:01 +0330

    style : add some class to header will be fix in screen

--- components/front/FrontHeader.vue
+++ components/front/FrontHeader.vue
@@ -9,6 +9,12 @@ export default {};
 <style lang="scss" scoped>
 .header {
   background: linear-gradient(180deg, #2d2db0 -65%, #2c8cf4 110%);
-  height: 100vh;
+  height: 100%;
+}
+
+@media screen and (max-width: 560px) {
+  .header {
+    height: auto !important;
+  }
 }
 </style>

7c3c7a0c2875637cad6dc90d9323f82104d69263
saeedsoltani <m.h.soltani1214@gmail.com>
2023-02-26 21:17:56 +0330

    style :change style of footer

--- components/front/TheFooter.vue
+++ components/front/TheFooter.vue
@@ -1,74 +1,63 @@
 <template>
-  <v-row dense>
-    <footer>
-      <v-container>
+  <client-only>
+    <v-row dense>
+      <footer>
         <v-row>
-          <v-col cols="12" md="4" lg="4" sm="4">
-            <div class="introduce">
-              <h4>درباره من</h4>
-              <p style="margin-top: 1rem">
-                برنامه نویس وطراح وب با بیش از 2 سال فعالیت در این زمینه مهارت
-                ها : php , laravel , vuejs , nuxtjs
-              </p>
-            </div>
-          </v-col>
-          <v-col cols="12" md="3" lg="3" sm="3">
-            <div class="content-footer">
-              <h4>محتوای سایت</h4>
-              <ul>
-                <li>
-                  <nuxt-link to="/front/files">فایل ها </nuxt-link>
-                </li>
-                <li>
-                  <nuxt-link to="/front/plans">طرح ها </nuxt-link>
-                </li>
-                <li>
-                  <nuxt-link to="/front/categories">دسته بندی</nuxt-link>
-                </li>
-              </ul>
-            </div>
-          </v-col>
-          <v-col cols="12" md="5" lg="5" sm="5">
-            <div class="contact-me">
-              <h4>ارتباط با من</h4>
-              <ul>
-                <li>
-                  ایمیل :
-                  <p>m.h.soltani1214@gmail.com</p>
-                </li>
-                <li>
-                  تلگرام :
-                  <p>
-                    <a href="https://t.me/mhsoltani1214" target="_blank"
-                      >mhsoltani1214@</a
-                    >
-                  </p>
-                </li>
-                <li>
-                  اینستاگرام :
-                  <p>
-                    <a
-                      href="https://www.instagram.com/saeedt.h1214"
-                      target="_blank"
-                      >saeedt.h1214@</a
-                    >
-                  </p>
-                </li>
-              </ul>
-            </div>
-          </v-col>
-          <v-col cols="12">
-            <v-divider color="#a38901"></v-divider>
+          <v-col cols="12" md="6" lg="6">
             <p class="copyright">
-              کليه حقوق محصولات و محتوای اين سایت متعلق به اینجانب می باشد و هر
-              گونه کپی برداری از محتوا و محصولات سایت غیر مجاز و بدون رضایت
-              اینجانب است.
+              کلیه حقوق این سایت متعلق به filymo.ir می باشد .
             </p>
           </v-col>
+          <v-col cols="12" md="6" lg="6">
+            <div class="socialite">
+              <a href="https://t.me/mhsoltani1214" target="_blank">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="2rem"
+                  height="2rem"
+                  preserveAspectRatio="xMidYMid meet"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="#fff"
+                    d="M12 20a8 8 0 1 0 0-16a8 8 0 0 0 0 16zm0 2C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10s-4.477 10-10 10zm-3.11-8.83l-2.498-.779c-.54-.165-.543-.537.121-.804l9.733-3.76c.565-.23.885.061.702.79l-1.657 7.82c-.116.557-.451.69-.916.433l-2.551-1.888l-1.189 1.148c-.122.118-.221.219-.409.244c-.187.026-.341-.03-.454-.34l-.87-2.871l-.012.008z"
+                  />
+                </svg>
+              </a>
+              <a href="https://www.instagram.com/saeedt.h1214" target="_blank">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="2rem"
+                  height="2rem"
+                  preserveAspectRatio="xMidYMid meet"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="#fff"
+                    d="M17.34 5.46a1.2 1.2 0 1 0 1.2 1.2a1.2 1.2 0 0 0-1.2-1.2Zm4.6 2.42a7.59 7.59 0 0 0-.46-2.43a4.94 4.94 0 0 0-1.16-1.77a4.7 4.7 0 0 0-1.77-1.15a7.3 7.3 0 0 0-2.43-.47C15.06 2 14.72 2 12 2s-3.06 0-4.12.06a7.3 7.3 0 0 0-2.43.47a4.78 4.78 0 0 0-1.77 1.15a4.7 4.7 0 0 0-1.15 1.77a7.3 7.3 0 0 0-.47 2.43C2 8.94 2 9.28 2 12s0 3.06.06 4.12a7.3 7.3 0 0 0 .47 2.43a4.7 4.7 0 0 0 1.15 1.77a4.78 4.78 0 0 0 1.77 1.15a7.3 7.3 0 0 0 2.43.47C8.94 22 9.28 22 12 22s3.06 0 4.12-.06a7.3 7.3 0 0 0 2.43-.47a4.7 4.7 0 0 0 1.77-1.15a4.85 4.85 0 0 0 1.16-1.77a7.59 7.59 0 0 0 .46-2.43c0-1.06.06-1.4.06-4.12s0-3.06-.06-4.12ZM20.14 16a5.61 5.61 0 0 1-.34 1.86a3.06 3.06 0 0 1-.75 1.15a3.19 3.19 0 0 1-1.15.75a5.61 5.61 0 0 1-1.86.34c-1 .05-1.37.06-4 .06s-3 0-4-.06a5.73 5.73 0 0 1-1.94-.3a3.27 3.27 0 0 1-1.1-.75a3 3 0 0 1-.74-1.15a5.54 5.54 0 0 1-.4-1.9c0-1-.06-1.37-.06-4s0-3 .06-4a5.54 5.54 0 0 1 .35-1.9A3 3 0 0 1 5 5a3.14 3.14 0 0 1 1.1-.8A5.73 5.73 0 0 1 8 3.86c1 0 1.37-.06 4-.06s3 0 4 .06a5.61 5.61 0 0 1 1.86.34a3.06 3.06 0 0 1 1.19.8a3.06 3.06 0 0 1 .75 1.1a5.61 5.61 0 0 1 .34 1.9c.05 1 .06 1.37.06 4s-.01 3-.06 4ZM12 6.87A5.13 5.13 0 1 0 17.14 12A5.12 5.12 0 0 0 12 6.87Zm0 8.46A3.33 3.33 0 1 1 15.33 12A3.33 3.33 0 0 1 12 15.33Z"
+                  />
+                </svg>
+              </a>
+              <a href="https://wa.link/3ehfzm" target="_blank">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="2rem"
+                  height="2rem"
+                  preserveAspectRatio="xMidYMid meet"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="#fff"
+                    d="m7.253 18.494l.724.423A7.953 7.953 0 0 0 12 20a8 8 0 1 0-8-8a7.95 7.95 0 0 0 1.084 4.024l.422.724l-.653 2.401l2.4-.655zM2.004 22l1.352-4.968A9.954 9.954 0 0 1 2 12C2 6.477 6.477 2 12 2s10 4.477 10 10s-4.477 10-10 10a9.954 9.954 0 0 1-5.03-1.355L2.004 22zM8.391 7.308c.134-.01.269-.01.403-.004c.054.004.108.01.162.016c.159.018.334.115.393.249c.298.676.588 1.357.868 2.04c.062.152.025.347-.093.537a4.38 4.38 0 0 1-.263.372c-.113.145-.356.411-.356.411s-.099.118-.061.265c.014.056.06.137.102.205l.059.095c.256.427.6.86 1.02 1.268c.12.116.237.235.363.346c.468.413.998.75 1.57 1l.005.002c.085.037.128.057.252.11c.062.026.126.049.191.066a.35.35 0 0 0 .367-.13c.724-.877.79-.934.796-.934v.002a.482.482 0 0 1 .378-.127c.06.004.121.015.177.04c.531.243 1.4.622 1.4.622l.582.261c.098.047.187.158.19.265c.004.067.01.175-.013.373c-.032.259-.11.57-.188.733a1.155 1.155 0 0 1-.21.302a2.378 2.378 0 0 1-.33.288a3.71 3.71 0 0 1-.125.09a5.024 5.024 0 0 1-.383.22a1.99 1.99 0 0 1-.833.23c-.185.01-.37.024-.556.014c-.008 0-.568-.087-.568-.087a9.448 9.448 0 0 1-3.84-2.046c-.226-.199-.435-.413-.649-.626c-.89-.885-1.562-1.84-1.97-2.742A3.47 3.47 0 0 1 6.9 9.62a2.729 2.729 0 0 1 .564-1.68c.073-.094.142-.192.261-.305c.127-.12.207-.184.294-.228a.961.961 0 0 1 .371-.1z"
+                  />
+                </svg>
+              </a>
+            </div>
+          </v-col>
         </v-row>
-      </v-container>
-    </footer>
-  </v-row>
+      </footer>
+    </v-row>
+  </client-only>
 </template>
 <script>
 export default {
@@ -82,36 +71,39 @@ export default {
 footer {
   width: 100%;
   margin-top: 1rem;
-  height: auto;
+  height: 70px;
   background: #253039;
   position: relative;
   bottom: 0;
   left: 0;
-}
-h4 {
-  color: #fff;
-}
-ul {
-  list-style: none;
-  margin-top: 0.5rem;
-  li,
-  a {
-    color: #a5b7c6;
-    text-decoration: none;
-    display: block;
-    margin-top: 0.5rem;
-  }
-}
-.contact-me ul li {
   display: flex;
-  align-items: baseline;
-  p {
-    margin-right: 0.5rem;
-  }
+  align-items: center;
 }
-.introduce {
-  &:first-child {
-    color: #a5b7c6;
+.socialite {
+  position: relative;
+  z-index: 100;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 1rem;
+  width: 100%;
+
+  a {
+    text-decoration: none;
+    background: transparent;
+    padding: 0.5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 8px;
+    transition: all 0.2s linear;
+    &:hover {
+      background: #fff;
+      border-radius: 50%;
+      svg path {
+        fill: #000;
+      }
+    }
   }
 }
 
@@ -122,5 +114,15 @@ ul {
 }
 .v-application p {
   margin-bottom: 0px !important;
+}
+
+@media screen and (max-width: 960px) {
+  .copyright p {
+    font-size: 0.9rem !important;
+  }
+
+  footer {
+    height: auto !important;
+  }
 }
 </style>

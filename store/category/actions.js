b99ff31d17bd5ded64adaed22395470757ae2c9a
saeedsoltani <m.h.soltani1214@gmail.com>
2023-04-03 10:12:55 +0330

    refactor actions in category

--- store/category/actions.js
+++ store/category/actions.js
@@ -1,30 +1,4 @@
 export default {
-  async fetchCategoriesForFilterFiles({ commit, state }) {
-    let params = {};
-    params["per_page"] = 4;
-    if (!Object.keys(state).length) {
-      console.log(state, "state");
-      let categories = await this.$axios
-        .get("frontend/categories", { params })
-        .then((res) => {
-          return res.data.data;
-        })
-        .catch((err) => console.log(err));
-      await commit("SET_CATEGORIES_FOR_FILTER_FILES", categories);
-    }
-  },
-
-  async fetchCategories({ commit, state }) {
-    if (!Object.keys(state).length) {
-      let categories = await this.$axios
-        .get("frontend/categories")
-        .then((res) => {
-          return res.data.data;
-        });
-      await commit("SET_CATEGORIES", categories);
-    }
-  },
-
   async fetchMenuBarCategories({ commit, state }) {
     let categories = await this.$axios
       .get("frontend/categories/menubar?include=subCategories")
